daf2f8bd5504afc7809824da7a618947a680bc5b
sakuraba_yusuke <>
2023-12-28 18:58:02 +0900

    #13 マップスクリーンのアウトラインを実装

---
+++ lib/Map/map_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:latlong2/latlong.dart';
+
+class MapScreen extends ConsumerWidget {
+  const MapScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:umi_sea/Component/typography/body_typography.dart';
 import 'package:umi_sea/Component/umi_sea_colors.dart';
+import 'package:umi_sea/Map/map_screen.dart';
 
 void main() {
-  runApp(const App());
+  runApp(
+    const ProviderScope(
+      child: App(),
+    ),
+  );
 }
 
 class App extends StatelessWidget {
@@ -12,18 +18,11 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // TODO: アプリタイトルが決まり次第適用
-      title: 'UmiSea',
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
-      home: Center(
-        child: Text(
-          "珊瑚礁",
-          style:
-              BodyTypography.largeBold.copyWith(color: UmiSeaColors.yellow500),
+        // TODO: アプリタイトルが決まり次第適用
+        title: 'UmiSea',
+        theme: ThemeData(
+          useMaterial3: true,
         ),
-      ),
-    );
+        home: const MapScreen());
   }
 }
