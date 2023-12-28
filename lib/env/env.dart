41d84211163160646f7e5d1c9d67c7df0de083d2
sakuraba_yusuke <>
2023-12-28 21:50:48 +0900

    #13 マップ画面を表示

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -5,5 +5,5 @@ part 'env.g.dart';
 @Envied(path: '.env')
 abstract class Env {
   @EnviedField(varName: 'MAPBOX_PUBLIC_ACCESS_TOKEN', obfuscate: true)
-  static String key = _Env.key;
+  static String mapboxPublicAccessToken = _Env.mapboxPublicAccessToken;
 }
