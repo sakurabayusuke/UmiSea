e4b0a1425bfbac8748d37381e4a551cff1c33f60
sakuraba_yusuke <>
2024-01-11 23:55:54 +0900

    #16 アプリで使い回すためのアイコンボタン追加

---
+++ lib/Component/icon/icon.dart
@@ -0,0 +1,6 @@
+enum Icon {
+  filter(path: "assets/images/icons/filter.svg");
+
+  final String path;
+  const Icon({required this.path});
+}
---
+++ lib/Component/icon/icon_size.dart
@@ -0,0 +1,7 @@
+enum IconSize {
+  middle(size: 20),
+  large(size: 30);
+
+  final double size;
+  const IconSize({required this.size});
+}
