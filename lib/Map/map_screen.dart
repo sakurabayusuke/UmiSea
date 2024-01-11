030e7353254ea7215221d2b9c82371d7fe1ead6e
sakuraba_yusuke <>
2024-01-11 23:56:34 +0900

    #16 マップ画面にフィルター画面表示のためのボタンを追加

--- lib/Map/map_screen.dart
+++ lib/Map/map_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 import 'package:umi_sea/env/env.dart';
+import 'package:umi_sea/Component/buttons/icon_button.dart' as atom;
+import 'package:umi_sea/Component/icon/icon.dart' as atom;
 
 class MapScreen extends ConsumerWidget {
   const MapScreen({Key? key}) : super(key: key);
@@ -9,16 +11,29 @@ class MapScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: MapWidget(
-        resourceOptions:
-            ResourceOptions(accessToken: Env.mapboxPublicAccessToken),
-        cameraOptions: CameraOptions(
-          center: Point(
-            coordinates: Position(139.7586677640881, 35.67369269880291),
-          ).toJson(),
-          zoom: 14,
-        ),
-        styleUri: MapboxStyles.MAPBOX_STREETS,
+      body: Stack(
+        alignment: AlignmentDirectional.center,
+        children: [
+          MapWidget(
+            resourceOptions:
+                ResourceOptions(accessToken: Env.mapboxPublicAccessToken),
+            cameraOptions: CameraOptions(
+              center: Point(
+                coordinates: Position(139.7586677640881, 35.67369269880291),
+              ).toJson(),
+              zoom: 14,
+            ),
+            styleUri: MapboxStyles.MAPBOX_STREETS,
+          ),
+          Positioned(
+            bottom: 160,
+            right: 24,
+            child: atom.IconButton(
+              icon: atom.Icon.filter,
+              onPressed: () {},
+            ),
+          ),
+        ],
       ),
     );
   }
