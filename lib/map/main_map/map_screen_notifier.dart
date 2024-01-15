2b8ea1dd1e165930c2e0d296aea2952842081751
sakuraba_yusuke <>
2024-01-15 16:24:12 +0900

    #21 使わないであろう部分をコメントアウト

--- lib/map/main_map/map_screen_notifier.dart
+++ lib/map/main_map/map_screen_notifier.dart
@@ -5,44 +5,48 @@ import 'package:umi_sea/map/main_map/map_screen_state.dart';
 import 'package:umi_sea/map/main_map/point_annotation_option_creator.dart';
 
 class MapScreenNotifier extends StateNotifier<MapScreenState> {
-  MapScreenNotifier() : super(MapScreenState(coralsPoint: []));
+  MapScreenNotifier() : super(MapScreenState(
+            // coralsPoint: [],
+            ));
 
   final CoralRepository coralRepository = CoralRepository();
 
   void onMapCreated(MapboxMap mapboxMap) async {
-    final pointManager =
-        await mapboxMap.annotations.createPointAnnotationManager();
+    // final pointManager =
+    //     await mapboxMap.annotations.createPointAnnotationManager();
     state = state.copyWith(
-        mapboxMap: mapboxMap, pointAnnotationManager: pointManager);
+      mapboxMap: mapboxMap,
+      // pointAnnotationManager: pointManager,
+    );
   }
 
   Future<void> putAllCorals() async {
     if (state.mapboxMap == null) {
       return;
     }
-    if (state.coralsPoint.isNotEmpty) {
-      return;
-    }
-    final pointCreator = PointAnnotationOptionCreator();
-    final coralsPoint = await pointCreator.getCoralAnnotations;
-    final coralsPointAnnotation =
-        await state.pointAnnotationManager!.createMulti(coralsPoint);
-    state = state.copyWith(coralsPoint: coralsPointAnnotation);
+    // if (state.coralsPoint.isNotEmpty) {
+    //   return;
+    // }
+    // final pointCreator = PointAnnotationOptionCreator();
+    // final coralsPoint = await pointCreator.getCoralAnnotations;
+    // final coralsPointAnnotation =
+    //     await state.pointAnnotationManager!.createMulti(coralsPoint);
+    // state = state.copyWith(coralsPoint: coralsPointAnnotation);
   }
 
   Future<void> deleteAllCorals() async {
-    if (state.mapboxMap == null) {
-      return;
-    }
-    if (state.coralsPoint.isEmpty) {
-      return;
-    }
-    final futures = <Future>[];
-    for (final point in state.coralsPoint) {
-      if (point == null) continue;
-      futures.add(state.pointAnnotationManager!.delete(point));
-    }
-    await Future.wait(futures);
-    state = state.copyWith(coralsPoint: []);
+    // if (state.mapboxMap == null) {
+    //   return;
+    // }
+    // if (state.coralsPoint.isEmpty) {
+    //   return;
+    // }
+    // final futures = <Future>[];
+    // for (final point in state.coralsPoint) {
+    //   if (point == null) continue;
+    //   futures.add(state.pointAnnotationManager!.delete(point));
+    // }
+    // await Future.wait(futures);
+    // state = state.copyWith(coralsPoint: []);
   }
 }
--- lib/map/main_map/map_screen_state.dart
+++ lib/map/main_map/map_screen_state.dart
@@ -3,24 +3,24 @@ import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 class MapScreenState {
   MapScreenState({
     this.mapboxMap,
-    this.pointAnnotationManager,
-    required this.coralsPoint,
+    // this.pointAnnotationManager,
+    // required this.coralsPoint,
   });
 
   late final MapboxMap? mapboxMap;
-  late final PointAnnotationManager? pointAnnotationManager;
-  final List<PointAnnotation?> coralsPoint;
+  // late final PointAnnotationManager? pointAnnotationManager;
+  // final List<PointAnnotation?> coralsPoint;
 
   MapScreenState copyWith({
     MapboxMap? mapboxMap,
-    PointAnnotationManager? pointAnnotationManager,
-    List<PointAnnotation?>? coralsPoint,
+    // PointAnnotationManager? pointAnnotationManager,
+    // List<PointAnnotation?>? coralsPoint,
   }) {
     return MapScreenState(
       mapboxMap: mapboxMap ?? this.mapboxMap,
-      pointAnnotationManager:
-          pointAnnotationManager ?? this.pointAnnotationManager,
-      coralsPoint: coralsPoint ?? this.coralsPoint,
+      // pointAnnotationManager:
+      //     pointAnnotationManager ?? this.pointAnnotationManager,
+      // coralsPoint: coralsPoint ?? this.coralsPoint,
     );
   }
 }
