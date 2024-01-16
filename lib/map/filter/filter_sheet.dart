d49618130e5d4a877d1afcfc366798f29d6babbf
sakuraba_yusuke <>
2024-01-17 04:36:13 +0900

    #29 filterSheet が表示中でも地図を操作できるようにするための変更

--- lib/map/filter/filter_sheet.dart
+++ lib/map/filter/filter_sheet.dart
@@ -1,32 +1,87 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:umi_sea/map/filter/filter.dart';
+import 'package:umi_sea/Component/umi_sea_colors.dart';
 import 'package:umi_sea/map/filter/filter_repository.dart';
 import 'package:umi_sea/map/filter/filter_sheet_notifier.dart';
+import 'package:umi_sea/map/filter/filter_sheet_state.dart';
 import 'package:umi_sea/map/filter/filter_tile.dart';
+import 'package:umi_sea/map/main_map/map_screen.dart';
 
 final filterSheetNotifierProvider =
-    StateNotifierProvider<FilterSheetNotifier, Map<Filter, bool>>((_) {
-  var filterRepository = FilterRepository();
-  return FilterSheetNotifier(filterRepository.getFilter);
+    StateNotifierProvider<FilterSheetNotifier, FilterSheetState>((_) {
+  final filters = FilterRepository().getFilter;
+  return FilterSheetNotifier(filters);
 });
 
 class FilterSheet extends ConsumerWidget {
-  const FilterSheet({super.key});
+  FilterSheet({super.key});
+
+  final DraggableScrollableController _controller =
+      DraggableScrollableController();
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     var list = <Widget>[];
-    var filters = ref.watch(filterSheetNotifierProvider);
-    for (var filter in filters.keys) {
+    var filterState = ref.watch(filterSheetNotifierProvider);
+    var mapState = ref.watch(mapScreenNotifierProvider.notifier);
+    for (var filter in filterState.filters.keys) {
       list.add(FilterTile(filter: filter));
     }
 
-    return SizedBox(
-      height: MediaQuery.sizeOf(context).height * 0.45,
-      child: GridView.count(
-        crossAxisCount: 4,
-        children: list,
+    return NotificationListener<DraggableScrollableNotification>(
+      onNotification: (DraggableScrollableNotification event) {
+        if (event.extent != event.minExtent) return false;
+        // 閉じる処理... アニメーションで
+        mapState.removeBottomSheet();
+        return false;
+      },
+      child: DraggableScrollableSheet(
+        minChildSize: 0.08,
+        initialChildSize: 0.5,
+        maxChildSize: 0.9,
+        controller: _controller,
+        shouldCloseOnMinExtent: true,
+        builder: (context, scrollController) {
+          return Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: UmiSeaColors.gray000),
+            child: Stack(
+              children: [
+                GridView.count(
+                  controller: scrollController,
+                  crossAxisCount: 4,
+                  children: list,
+                ),
+                GestureDetector(
+                  onVerticalDragUpdate: (details) {
+                    final pixel = _controller.pixels - details.delta.dy;
+                    final size = _controller.pixelsToSize(pixel);
+                    _controller.jumpTo(size);
+                  },
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          width: 100,
+                          height: 7,
+                          decoration: BoxDecoration(
+                            color: UmiSeaColors.gray300,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
