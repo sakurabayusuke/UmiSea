2e1a28bc27667057cc5a97dd19393f2609c55d78
sakuraba_yusuke <>
2024-01-25 02:58:29 +0900

    #65 旧スプラッシュ画面の管理のコードを削除

--- lib/map/main_map/map_screen_state.dart
+++ lib/map/main_map/map_screen_state.dart
@@ -1,22 +1,18 @@
 class MapScreenState {
   const MapScreenState({
     required this.initialized,
-    required this.splashIsEnd,
     required this.bottomSheetIsAnimating,
   });
 
   final bool initialized;
-  final bool splashIsEnd;
   final bool bottomSheetIsAnimating;
 
   MapScreenState copyWith({
     bool? initialized,
-    bool? splashIsEnd,
     bool? bottomSheetIsAnimating,
   }) {
     return MapScreenState(
       initialized: initialized ?? this.initialized,
-      splashIsEnd: splashIsEnd ?? this.splashIsEnd,
       bottomSheetIsAnimating:
           bottomSheetIsAnimating ?? this.bottomSheetIsAnimating,
     );
